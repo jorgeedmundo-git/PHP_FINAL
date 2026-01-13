4a20124ce57a5269f716d683ef06843b1b2c670f
jorgeedmundo-git <jorgeedmundo1991@gmail.com>
2026-01-13 00:11:57 +0000

    Agregadas entidades Ponente y Asistente a la API

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 // Agregar el controlador EventoController
 use App\Http\Controllers\EventoController;
+use App\Http\Controllers\PonenteController; 
+use App\Http\Controllers\AsistenteController;
 
 /**
  * Rutas para el recurso Evento.
@@ -19,3 +21,6 @@ Route::get('/eventos/{id}', [EventoController::class, 'show']);
 Route::put('/eventos/{evento}', [EventoController::class, 'update']);
 // Eliminar un evento específico
 Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
+
+Route::apiResource('ponentes', PonenteController::class);
+Route::apiResource('asistentes', AsistenteController::class);
